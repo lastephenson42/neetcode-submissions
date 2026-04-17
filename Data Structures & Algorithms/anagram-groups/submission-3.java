2760f4596fb5c95161d37418efb7ad78d6193941
lastephenson <122657339+lastephenson42@users.noreply.github.com>
2026-04-17 17:38:22 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        // make a map with entries (sorted word, list of words)
+        // key is the word sorted
+
+        // to add entries to the map
+        // sort the word, search for sorted word as a key
+        // if the key exists, add it to the list
+        // otherwise make a new list
+
+        HashMap<String,List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            // convert to char array
+            char[] wordChar = word.toCharArray();
+            Arrays.sort(wordChar);
+            String sortedWord = new String(wordChar);
+
+            // if no matching key is found
+            if (!map.containsKey(sortedWord)) {
+                // create new empty list entry
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            // add word to the map entry
+            map.get(sortedWord).add(word);
+        }
+
+        // return a List of each list in map
+        // use the values() method for this
+        return new ArrayList<>(map.values());
+    }
+}
