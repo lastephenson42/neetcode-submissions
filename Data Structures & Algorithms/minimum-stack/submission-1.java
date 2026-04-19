8075eca1f2dbce2cf5f512a4c060aec968a0db72
lastephenson <122657339+lastephenson42@users.noreply.github.com>
2026-04-19 01:14:19 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,37 @@
+class MinStack {
+    private record Pair<A, B>(A min, B val) {}
+    private Deque<Pair<Integer, Integer>> stack;
+
+    public MinStack() {
+        stack = new ArrayDeque<Pair<Integer, Integer>>();
+    }
+
+    public void push(int val) {
+        Pair<Integer, Integer> elem;
+
+        if (stack.isEmpty()) {
+            elem = new Pair(val, val);
+        } else {
+            int min = stack.peekFirst().min;
+            min = (min < val) ? min : val;
+            elem = new Pair(min, val);
+        }
+
+        stack.offerFirst(elem);
+    }
+
+    // always called on non-empty stacks
+    public void pop() {
+        stack.remove();
+    }
+
+    // always called on non-empty stacks
+    public int top() {
+        return stack.peekFirst().val;
+    }
+
+    // always called on non-empty stacks
+    public int getMin() {
+        return stack.peekFirst().min;
+    }
+}
